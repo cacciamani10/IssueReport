82cb25922dc22a82daa299d40b686e21abd1c7cd
Jacob Cacciamani <cacciamani10@gmail.com>
2019-10-27 19:45:43 -0400

    added new routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // Includes
 const express = require('express');
 const { Client } = require('pg');
-
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+const testUser ='6f805bae-6988-486c-a4ac-039f7cc98b5b';
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -21,9 +23,33 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   });
 
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>');
+    res.send(path.join(__dirname, 'public', index));
+});
+
+
+app.get('/getIssues', (req, res) => {
+  const string = 'SELECT row_to_json(*) FROM tickets';
+  client.query(string, (err, data) => {
+    if (err)
+      res.writeHead(500);
+    else {
+        res.json(data);
+    }
+  })
+});
+
+app.post('/create', (req, res) => {
+  console.log(req);
+  const string = 'INSERT INTO tickets(ticket_id, created_by, ticket_subject, ticket_description, resolved) VALUES($1, $2, $3, $4, $5) RETURNING *';
+  const values = [ uuidv4(), testUser, req.subject, req,description, false];
+  if (err) {
+    console.log(err.stack);
+  }
+  else {
+    console.log('Added', res.rows[0]);
+  }
 });
 
 app.listen(PORT);
