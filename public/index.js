cde9d205978edc86796aaa140dcd4126acb3d346
Jacob Cacciamani <cacciamani10@gmail.com>
2019-10-27 23:20:01 -0400

    client fetch url 4

--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
 //const url = window.location.hostname;
 fetch('/getIssues')
     .then(res => {
+        if (res.status !== 200) {
+            console.log(res.status);
+            return;
+        }
         res.json()
         .then(data => {
             console.log(data);
