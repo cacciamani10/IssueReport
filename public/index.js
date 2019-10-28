0907430269889fe3abe38c9495361d1b4656ad96
Jacob Cacciamani <cacciamani10@gmail.com>
2019-10-28 02:44:47 -0400

    add search functionality

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-//const url = window.location.hostname;
+const issues = [];
 fetch('/getIssues')
     .then(res => {
         if (res.status !== 200) {
@@ -9,6 +9,7 @@ fetch('/getIssues')
             let tickets = document.getElementById('issues');
             tickets.innerHTML = '';
             for (let item of data) {
+                issues.push(item);
                 tickets.innerHTML += 
                 `<div class="card" style="margin: 12px;">
                     <div class="card-header">
@@ -28,3 +29,34 @@ fetch('/getIssues')
     .catch(err => {
         console.log(err);
     });
+
+function filterItems() {
+    let found;
+    const search = document.getElementById('search-bar');
+    let tickets = document.getElementById('issues');
+    tickets.innerHTML = '';
+    for (let item of tickets) {
+        found = false;
+        for (let [key, value] of Object.entries(item)) {
+            if (value.includes(search)) {
+                found = true;
+                break;
+            }
+            if(found) {
+            tickets.innerHTML += 
+                `<div class="card" style="margin: 12px;">
+                    <div class="card-header">
+                        Ticket ID: ${item.ticket_id}
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${item.ticket_subject}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${item.created_by}</h6>
+                        <p class="card-text">${item.ticket_description}</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>`
+            }
+        }
+    }
+}
