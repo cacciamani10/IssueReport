28aba803d981fe233eac67415ba3dde521549d78
Jacob Cacciamani <cacciamani10@gmail.com>
2019-10-28 18:00:08 -0400

    frontent resolve bootstrap class troubleshooting

--- public/index.js
+++ public/index.js
@@ -10,31 +10,7 @@ fetch('/getIssues')
             tickets.innerHTML = '';
             for (let item of data) {
                 issues.push(item);
-                tickets.innerHTML += 
-                `<div class="card" style="margin: 30px 12px;">
-                    <div class="card-header" style="font-weight: 500;">
-                        Ticket ID: ${item.ticket_id}
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">${item.ticket_subject}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Created by: ${item.created_by}</h6>
-                        <p class="card-text">${item.ticket_description}</p>`;
-                if (!item.resolved) {
-                    tickets.innerHTML +=
-                        `<span class="badge badge-warning">Unresolved</span>
-                        <a href="#" class="card-link">Resolve</a>
-                    </div>
-                </div>`;
-                }
-                else {
-                    tickets.innerHTML +=
-                        `<span class="badge badge-success">Resolved</span>
-                        <div class="card-footer text-muted">
-                            Resolved by: ${item.resolved_by} on ${resolved_on}
-                        </div>
-                    </div>
-                </div>`
-                }
+                tickets.innerHTML += issueToString(item);
             }
         });
     })
@@ -56,31 +32,38 @@ function filterItems() {
             }
         }
         if(found) {
-            tickets.innerHTML += 
-                `<div class="card" style="margin: 30px 12px;">
-                    <div class="card-header" style="font-weight: 500;">
-                        Ticket ID: ${item.ticket_id}
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">${item.ticket_subject}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Created by: ${item.created_by}</h6>
-                        <p class="card-text">${item.ticket_description}</p>`;
-            if (!item.resolved) {
-                tickets.innerHTML +=
-                    `<span class="badge badge-warning">Unresolved</span>
-                    <a href="#" class="card-link">Resolve</a>
-                </div>
-            </div>`;
-            }
-            else {
-                tickets.innerHTML +=
-                    `<span class="badge badge-success">Resolved</span>
-                    <div class="card-footer text-muted">
-                        Resolved by: ${item.resolved_by}
-                    </div>
-                </div>
-            </div>`
-            }
+            tickets.innerHTML += issueToString(item);
+            console.log(issueToString(item));
         }
     }
+}
+
+function issueToString(item) {
+    let res = '';
+    res += 
+    `<div class="card" style="margin: 30px 12px;">
+        <div class="card-header" style="font-weight: 500;">
+            Ticket ID: ${item.ticket_id}
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">${item.ticket_subject}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Created by: ${item.created_by}</h6>
+            <p class="card-text">${item.ticket_description}</p>`;
+    if (!item.resolved) {
+        res +=
+            `<span class="badge badge-warning">Unresolved</span>
+            <a href="#" class="card-link">Resolve</a>
+        </div>
+    </div>`;
+    }
+    else {
+        res +=
+            `<span class="badge badge-success">Resolved</span>
+            <div class="card-footer text-muted">
+                Resolved by: ${item.resolved_by} on ${resolved_on}
+            </div>
+        </div>
+    </div>`
+    }
+    return res;
 }
