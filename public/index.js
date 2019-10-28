ea441d601d1a2fcf7f4721647c229576e44fe003
Jacob Cacciamani <cacciamani10@gmail.com>
2019-10-28 00:05:49 -0400

    add bootstrap cards

--- public/index.js
+++ public/index.js
@@ -6,8 +6,22 @@ fetch('/getIssues')
             return;
         }
         res.json().then(data => {
+            let tickets = document.getElementById('issues');
+            tickets.innerHTML = '';
             for (let item of data) {
-                console.log(item);
+                tickets.innerHTML += 
+                `<div class="card">
+                    <div class="card-header">
+                        ${item.ticket_id}
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${item.ticket_subject}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${item.created_by}</h6>
+                        <p class="card-text">${item.ticket_description}</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>`
             }
         });
     })
